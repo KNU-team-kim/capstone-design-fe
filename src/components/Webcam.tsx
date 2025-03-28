04ed48a5e5796796898c4df331b3c2e6503eae21
jsolbin <jsolbin0724@gmail.com>
2025-03-28 23:02:22 +0900

    chore: ESLint, Prettier 설정 변경

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -1,21 +1,36 @@
-'use client';
+'use client'
 
-import { useWebRTC } from "@/hooks/useWebRTC";
+import { useWebRTC } from '@/hooks/useWebRTC'
+
 const Webcam = () => {
-  const { videoRef, isConnected } = useWebRTC();
+  const { videoRef } = useWebRTC()
 
   return (
     <div>
       <div>
         <h1>WebRTC 연동</h1>
-        <video ref={(el) => { videoRef.current[0] = el; }} autoPlay playsInline width="300" />
+        <video
+          ref={(el) => {
+            videoRef.current[0] = el
+          }}
+          autoPlay
+          playsInline
+          width="300"
+        />
       </div>
       <div>
         <h1>OBS 가상 카메라</h1>
-        <video ref={(el) => { videoRef.current[1] = el; }} autoPlay playsInline width="300" />
+        <video
+          ref={(el) => {
+            videoRef.current[1] = el
+          }}
+          autoPlay
+          playsInline
+          width="300"
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Webcam;
+export default Webcam
