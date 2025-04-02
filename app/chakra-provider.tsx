73043fa56f3d46861f01a1182e392224178fc2cf
jsolbin <jsolbin0724@gmail.com>
2025-04-02 13:48:07 +0900

    feat: DirectionTag 컴포넌트 추가

---
+++ app/chakra-provider.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({})
+
+export default function ChakraWrapper({ children }: { children: ReactNode }) {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+}
---
+++ src/components/DirectionTag.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Badge } from '@chakra-ui/react'
+
+type Direction = 'front' | 'behind' | 'left' | 'right'
+
+interface Props {
+  direction: Direction
+}
+
+const directionColorMap: Record<Direction, string> = {
+  // 색상 팔레트 추가 후 수정하기
+  front: 'gray',
+  behind: 'gray',
+  left: 'gray',
+  right: 'gray',
+}
+
+const DirectionTag = ({ direction }: Props) => {
+  return (
+    <Badge
+      // 색상 다시 수정하기
+      colorScheme={directionColorMap[direction]}
+      borderRadius="lg"
+      px={4}
+      py={1}
+      fontWeight="medium"
+      fontSize="sm"
+      textTransform="none"
+    >
+      {direction}
+    </Badge>
+  )
+}
+
+export default DirectionTag
