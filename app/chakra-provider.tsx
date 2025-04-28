1322bfd7c0772e2a2a2a78f87884d0c732661d5c
jsolbin <jsolbin0724@gmail.com>
2025-04-28 16:55:11 +0900

    chore: downgrade chakra-ui

--- app/chakra-provider.tsx
+++ app/chakra-provider.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { ReactNode } from 'react'
-
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-
-const theme = extendTheme({})
+import { ChakraProvider } from '@chakra-ui/react'
 
 export default function ChakraWrapper({ children }: { children: ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  return (
+    <ChakraProvider>
+      {children}
+    </ChakraProvider>
+  )
 }
