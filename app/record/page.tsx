c00c3355116c735cc58af89c25271eb757bd0282
jsolbin <jsolbin0724@gmail.com>
2025-04-30 17:04:28 +0900

    refactor: 페이지 명 progress -> record

--- app/record/page.tsx
+++ app/record/page.tsx
@@ -1,7 +1,7 @@
-export default function ProgressPage() {
+export default function RecordPage() {
   return (
     <main>
-      <h1>Progress Page</h1>
+      <h1>Record Page</h1>
       <p>이곳은 로그 페이지입니다.</p>
     </main>
   )
